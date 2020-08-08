eae08481b6dd63be228ad28ef94463a5019e841a
Luc Patiny <luc@patiny.com>
2020-08-08 08:06:52 +0200

    feat(Arduino): add fan control

---
+++ arduino/BioMain/Fan.ino
@@ -0,0 +1,33 @@
+#ifdef THR_FAN
+
+
+NIL_WORKING_AREA(waThread_FAN, 0); //tune the allocated mem (here extra is provided)
+NIL_THREAD(Thread_FAN, arg)
+{
+  nilThdSleepMilliseconds(1000);
+  pinMode(OUT_FAN, OUTPUT);
+
+
+  while (true) {
+    setFan();
+    nilThdSleepMilliseconds(2000); 
+  }
+}
+
+/*Temperature PID Control addressing relay*/
+
+void setFan() {
+  int temperature = getParameter(PARAM_TEMP_PCB);
+  if (temperature > 6000) {
+    analogWrite(OUT_FAN, 255);
+  } else if (temperature > 4000) {
+    analogWrite(OUT_FAN, 170);
+  } else if (temperature > 3000) {
+    analogWrite(OUT_FAN, 100);
+  } else {
+    analogWrite(OUT_FAN, 0);
+  }
+
+}
+
+#endif
